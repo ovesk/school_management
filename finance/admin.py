4770d9f4a7544579859fcba4c38ffb4263a9196b
Oves <oves@Ovess-MacBook-Pro.local>
2024-12-23 02:55:00 +0530

    Added models to admin

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Organization, Receipt
 
-# Register your models here.
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'contact_number')
+    search_fields = ('name', 'address','contact_number')
+    list_filter = ('name',)
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('organization', 'recipient_name', 'amount', 'description')
+    search_fields = ('recipient_name', 'organization__name')
+    list_filter = ('organization',)
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Receipt, ReceiptAdmin)
